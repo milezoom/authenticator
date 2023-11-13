1a7136ab7aa8f874e93dbc33b1a0a22191602a57
milezoom <010.muslim@gmail.com>
2023-11-13 14:48:45 +0700

    init project egui

--- src/main.rs
+++ src/main.rs
@@ -1,60 +1,23 @@
-#![windows_subsystem = "windows"]
+#![cfg_attr(not(debug_assertions), windows_subsystem = "windows")] // hide console window on Windows in release
+use eframe::egui;
 
-use iced::pure::widget::{Button, Column, Container, Row, Text};
-use iced::pure::Sandbox;
-use iced::{window, Settings};
-
-struct Counter {
-    count: i32,
-}
-
-#[derive(Debug, Clone, Copy)]
-enum CounterMessage {
-    Increment,
-    Decrement,
-}
-
-impl Sandbox for Counter {
-    type Message = CounterMessage;
-    fn new() -> Self {
-        Counter { count: 0 }
-    }
-    fn title(&self) -> String {
-        String::from("Counter App")
-    }
-    fn update(&mut self, message: Self::Message) {
-        match message {
-            CounterMessage::Increment => self.count += 1,
-            CounterMessage::Decrement => self.count -= 1,
-        }
-    }
-    fn view(&self) -> iced::pure::Element<'_, Self::Message> {
-        let app_name = Text::new("Authenticator");
-        let btn_add = Button::new("Hello");
-        let header = Row::new().push(app_name).push(btn_add);
-
-        let label = Text::new(format!("Count: {}", self.count));
-        let incr = Button::new("Increment").on_press(CounterMessage::Increment);
-        let decr = Button::new("Decrement").on_press(CounterMessage::Decrement);
-        let col = Column::new().push(header).push(incr).push(label).push(decr);
-        Container::new(col)
-            .align_x(iced::alignment::Horizontal::Center)
-            .align_y(iced::alignment::Vertical::Top)
-            .width(iced::Length::Fill)
-            .height(iced::Length::Fill)
-            .into()
-    }
-}
-
-fn main() -> Result<(), iced::Error> {
-    let settings = Settings {
-        window: window::Settings {
-            size: (400, 600),
-            resizable: (false),
-            decorations: (true),
-            ..Default::default()
-        },
+fn main() -> Result<(), eframe::Error> {
+    let options = eframe::NativeOptions {
+        initial_window_size: Some(egui::vec2(400.0, 600.0)),
         ..Default::default()
     };
-    Counter::run(settings)
+
+    eframe::run_simple_native("Authenticator", options, move |ctx, _frame| {
+        egui::TopBottomPanel::top("heading").show(ctx, |ui| {
+            ui.columns(2, |col| {
+                col[0].heading("Authenticator");
+                col[1].add_sized([10.0, 10.0], egui::Button::new("Add"));
+            });
+        });
+        egui::CentralPanel::default().show(ctx, |ui| {
+            ui.add(egui::Label::new("Hello world!"));
+            ui.label("some long text here, okay");
+            if ui.button("click me").clicked() {}
+        });
+    })
 }

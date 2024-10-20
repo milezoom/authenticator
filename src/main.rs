33afdd615a146aae0e109e95b00092e443c04e95
Muslim <010.muslim@gmail.com>
2024-10-21 00:34:17 +0700

    add code to create simple counter

--- src/main.rs
+++ src/main.rs
@@ -1,60 +1,42 @@
-#![windows_subsystem = "windows"]
+use iced::widget::{button, column, text, Column};
+use iced::Center;
 
-use iced::pure::widget::{Button, Column, Container, Row, Text};
-use iced::pure::Sandbox;
-use iced::{window, Settings};
+pub fn main() -> iced::Result {
+    iced::application("A cool counter", Counter::update, Counter::view)
+        .window_size((400.0, 600.0))
+        .run()
+}
 
+#[derive(Default)]
 struct Counter {
-    count: i32,
+    value: i64,
 }
 
 #[derive(Debug, Clone, Copy)]
-enum CounterMessage {
+enum Message {
     Increment,
     Decrement,
 }
 
-impl Sandbox for Counter {
-    type Message = CounterMessage;
-    fn new() -> Self {
-        Counter { count: 0 }
-    }
-    fn title(&self) -> String {
-        String::from("Counter App")
-    }
-    fn update(&mut self, message: Self::Message) {
+impl Counter {
+    fn update(&mut self, message: Message) {
         match message {
-            CounterMessage::Increment => self.count += 1,
-            CounterMessage::Decrement => self.count -= 1,
+            Message::Increment => {
+                self.value += 1;
+            }
+            Message::Decrement => {
+                self.value -= 1;
+            }
         }
     }
-    fn view(&self) -> iced::pure::Element<'_, Self::Message> {
-        let app_name = Text::new("Authenticator");
-        let btn_add = Button::new("Hello");
-        let header = Row::new().push(app_name).push(btn_add);
 
-        let label = Text::new(format!("Count: {}", self.count));
-        let incr = Button::new("Increment").on_press(CounterMessage::Increment);
-        let decr = Button::new("Decrement").on_press(CounterMessage::Decrement);
-        let col = Column::new().push(header).push(incr).push(label).push(decr);
-        Container::new(col)
-            .align_x(iced::alignment::Horizontal::Center)
-            .align_y(iced::alignment::Vertical::Top)
-            .width(iced::Length::Fill)
-            .height(iced::Length::Fill)
-            .into()
+    fn view(&self) -> Column<Message> {
+        column![
+            button("Increment").on_press(Message::Increment),
+            text(self.value).size(50),
+            button("Decrement").on_press(Message::Decrement)
+        ]
+        .padding(20)
+        .align_x(Center)
     }
-}
-
-fn main() -> Result<(), iced::Error> {
-    let settings = Settings {
-        window: window::Settings {
-            size: (400, 600),
-            resizable: (false),
-            decorations: (true),
-            ..Default::default()
-        },
-        ..Default::default()
-    };
-    Counter::run(settings)
 }
